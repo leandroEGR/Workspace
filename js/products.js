766495281be7bbce923ce9892c1a45b93c800b40
leandroegr <88410216+leandroegr@users.noreply.github.com>
2021-08-22 17:48:51 -0300

    Se cargan archivos actualizados
    Carga de lista de productos y login

--- js/products.js
+++ js/products.js
@@ -2,5 +2,41 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-
+    let arrayProducts = {}; // se declara objeto para trabajar JSON.
+    // Se realiza consulta para traer listado de productos.
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok"){          
+            arrayProducts = resultObj.data;
+        }
+    // Función que muestra listado de artículos.    
+    function showProductsList(){
+        
+        let htmlContentToAppend = "";
+        for(let i = 0; i < arrayProducts.length; i++){
+            let products = arrayProducts[i];
+    
+            
+    
+                htmlContentToAppend += `
+                <a href="product-info.html" class="list-group-item list-group-item-action">
+                    <div class="row">
+                        <div class="col-3">
+                            <img src="` + products.imgSrc + `" alt="` + products.description + `" class="img-thumbnail">
+                        </div>
+                        <div class="col">
+                            <div class="d-flex w-100 justify-content-between">
+                                <h4 class="mb-1">`+ products.name +`</h4>
+                                <small class="text-muted">` + products.soldCount + ` artículos</small>
+                            </div>
+                            <p class="mb-1">` + products.description + `</p>
+                        </div>
+                    </div>
+                </a>
+                `
+            
+            document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+        } 
+}
+showProductsList() 
+});
 });
