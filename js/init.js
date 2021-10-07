d798f4da44a55969043feee893e3c921a1ac1854
leandroegr <88410216+leandroegr@users.noreply.github.com>
2021-10-07 12:32:11 -0300

    Barra de navegación responsive

--- js/init.js
+++ js/init.js
@@ -30,24 +30,32 @@ function viewNav() {
       document.body.innerHTML += navbarCont ;
   } else {
   
-  let navbarCont = ` <nav class='site-header sticky-top py-1 bg-dark'>
-    <div class="container d-flex flex-column flex-md-row justify-content-between">
-      <a class="py-2 d-none d-md-inline-block" href="main.html">Inicio</a>
-      <a class="py-2 d-none d-md-inline-block" href="categories.html">Categorías</a>
-      <a class="py-2 d-none d-md-inline-block" href="products.html">Productos</a>
-      <a class="py-2 d-none d-md-inline-block" href="sell.html">Vender</a>
-      <a class="py-2 d-none d-md-inline-block" href="cart.html">Mi carrito</a>
-      <div class="dropdown">
-        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+  let navbarCont = ` <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+  <a href="main.html" style="margin-right: 25%"><img src="/img/icon.png" width="40" height="40" alt="Error!"><a/>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <button class="btn btn-outline-secondary my-2 my-sm-0"><a href="main.html" style="decoration:none; color:white;">Inicio</a> <span class="sr-only">(current)</span></button>
+      <button class="btn btn-outline-secondary my-2 my-sm-0"><a href="categories.html" style="decoration:none; color:white;">Categorias</a> <span class="sr-only">(current)</span></button>
+      <button class="btn btn-outline-secondary my-2 my-sm-0"><a href="products.html" style="decoration:none; color:white;">Productos</a> <span class="sr-only">(current)</span></button>
+      <button class="btn btn-outline-secondary my-2 my-sm-0"><a href="sell.html" style="decoration:none; color:white;">Vender</a> <span class="sr-only">(current)</span></button>
+      <button class="btn btn-outline-secondary my-2 my-sm-0"><a href="cart.html" style="decoration:none; color:white;">Carrito</a> <span class="sr-only">(current)</span></button>
+      
+      <div class="dropdown mr-sm-2">
+        <button class="btn btn-outline-info my-2 my-sm-0" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         ` + userName + `</button>
-        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
           <a class="dropdown-item" href="my-profile.html">Mi perfil</a>
-          
           <a class="dropdown-item" onclick = "clearSession()" href="index.html">Cerrar sesión</a>
         </div>
       </div>
     </div>
-  </nav> `
+    </div>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  
+  </div>
+</nav> `
   document.body.innerHTML += navbarCont ; }
 }
 
